1bd1ca6051e1e018ceac4b2c178172dc34ba2794
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-24 23:21:49 -0500

    creación de función getALl que obtendrá todos el historial de movimientos

--- client/api/services/inventoryMovementService.ts
+++ client/api/services/inventoryMovementService.ts
@@ -5,5 +5,13 @@ const BASE_URL = '/inventory-movements';
 
 export const InventoryMovementService = {
 
+    /**
+     * Obtener todo el historial de movimientos (Kardex Global).
+     */
+    getAll: async (): Promise<InventoryMovement[]> => {
+        const response = await http.get<InventoryMovement[]>(BASE_URL);
+        return response.data;
+    },
+
 
 };
