6a8ab6b6751fe39d26643ef5f61529cb60bd07ff
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-27 00:07:47 -0500

    definciión de constante para endpoint de alerts

---
+++ client/api/services/alertService.ts
@@ -0,0 +1,4 @@
+import { http } from '../http';
+import { Alert, AlertStatus, StockAlertCalculated, UpdateAlertStatusPayload } from '../../types/inventory/alerts';
+
+const API_BASE_URL = '/inventory/alerts';
