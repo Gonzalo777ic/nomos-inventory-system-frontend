6139921104f5750ac5d2f01c3389e32803265aad
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-29 00:11:09 -0500

    creación de servicio de announcement y definicón de constante que tendrá el valor base del endpoint

---
+++ client/api/services/announcementService.ts
@@ -0,0 +1,4 @@
+import { httpStore } from "../httpStore";
+import { Announcement, AnnouncementDTO } from "../../types/store/announcement";
+
+const ANNOUNCEMENTS_URL = 'http://localhost:8083/api/store/announcements';
