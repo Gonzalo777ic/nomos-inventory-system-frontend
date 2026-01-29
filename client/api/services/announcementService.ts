477b2446bf83b4d0ddcfbb648bfed79bdc7875ee
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-29 00:11:37 -0500

    creación de función asíncrona get all para obtener todos los anuncios

--- client/api/services/announcementService.ts
+++ client/api/services/announcementService.ts
@@ -2,3 +2,18 @@ import { httpStore } from "../httpStore";
 import { Announcement, AnnouncementDTO } from "../../types/store/announcement";
 
 const ANNOUNCEMENTS_URL = 'http://localhost:8083/api/store/announcements';
+
+
+
+export const AnnouncementService = {
+    
+    /**
+     * Obtener todos los anuncios (Vista de Administrador).
+     */
+    getAll: async (): Promise<Announcement[]> => {
+        const response = await httpStore.get<Announcement[]>(ANNOUNCEMENTS_URL);
+        return response.data;
+    },
+
+    
+};
