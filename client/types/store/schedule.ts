8df47fb0e4bcf9f3bce4eb012dc9851db6f57eb9
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-30 22:01:40 -0500

    creación de store schedule

--- client/types/store/schedule.ts
+++ client/types/store/schedule.ts
@@ -1,1 +1,9 @@
 export type DayOfWeek = 'MONDAY' | 'TUESDAY' | 'WEDNESDAY' | 'THURSDAY' | 'FRIDAY' | 'SATURDAY' | 'SUNDAY';
+
+export interface StoreSchedule {
+    id: number;
+    dayOfWeek: DayOfWeek;
+    openingTime: string; 
+    closingTime: string;
+    isOpen: boolean;
+}
