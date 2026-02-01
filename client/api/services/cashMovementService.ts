d856af3e2851dacfa77cdd8a4d9c73e019aa74ee
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-02-01 01:04:58 -0500

    creación de archivo cashmovement service y creación de constante para la ruta del endpoint de la entidad

---
+++ client/api/services/cashMovementService.ts
@@ -0,0 +1,9 @@
+import { httpStore } from "../httpStore";
+import { CashMovement, CashMovementPayload, CashMovementFilter } from "../../types/store/cash";
+
+const CASH_URL = 'http://localhost:8083/api/store/cash-movements';
+
+export const CashMovementService = {
+    
+    
+};
