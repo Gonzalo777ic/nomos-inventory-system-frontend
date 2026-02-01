33ab501ec4cd707dd41f33d3b1c6b076078f5001
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-02-01 01:02:52 -0500

    creación de tipos cashmovement type y cash movement status

--- client/types/store/cash.ts
+++ client/types/store/cash.ts
@@ -1,2 +1,5 @@
 import { Sale, PaymentMethodConfig } from "../store";
 
+export type CashMovementType = 'INCOME' | 'EXPENSE';
+export type CashMovementStatus = 'PROCESSED' | 'PENDING' | 'ANNULLED';
+
